4b5816065e1e7e90fbec156e2622fe075cc6e8ab
Sarah Garcia <garciasarah2099@gmail.com>
2025-12-12 16:47:05 -0500

    added cleanup of working dir

--- clean_up.sh
+++ clean_up.sh
@@ -1,8 +1,15 @@
 #For resetting nextflow directory during debugging
 
+rm -f .nextflow*
+rm -f logs
+
+cd /lustre/or-scratch/cades-bsd/$USER
+
 if [ -d "results" ]; then rm -r results; fi
 
 if [ -d "work" ];    then rm -r work; fi
+
+if [ -d "stash" ];   then rm -r stash; fi
 
 rm -f .nextflow*
 
