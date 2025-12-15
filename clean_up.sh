ace41af6a40d26b6b7c62abb46fc44b90dee536e
Sarah Garcia <garciasarah2099@gmail.com>
2025-12-15 11:43:54 -0500

    edit

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,7 @@
 #For resetting nextflow directory during debugging
 
-if [ -d ".nextflow*" ]; then rm -r .nextflow*; fi
-if [ -d "logs" ];       then rm -r logs; fi
+if [ -d ".nextflo*" ]; then rm -r .nextflo*; fi
+if [ -d "logs" ];      then rm -r logs; fi
 
 cd /lustre/or-scratch/cades-bsd/$USER
 
@@ -10,7 +10,5 @@ if [ -d "results" ]; then rm -r results; fi
 if [ -d "work" ];    then rm -r work; fi
 
 if [ -d "stash" ];   then rm -r stash; fi
-
-rm -f .nextflow*
 
 echo "\n Reset! \n"
