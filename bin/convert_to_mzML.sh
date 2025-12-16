0d11c65be30c58cc38236c18f0f70a86536a66a2
Sarah Garcia <garciasarah2099@gmail.com>
2025-12-16 17:19:58 -0500

    corrected image name

--- bin/convert_to_mzML.sh
+++ bin/convert_to_mzML.sh
@@ -19,7 +19,7 @@ echo "[INFO] Output directory: $OUTDIR"
 echo "[INFO] Output file: $OUTFILE"
 
 # Singularity container image path
-CONTAINER_IMAGE="/lustre/or-scratch/cades-bsd/jkg/cache/thermorawfileparser-1.4.5--h05cac1d_1.sif"
+CONTAINER_IMAGE="/lustre/or-scratch/cades-bsd/jkg/cache/quay.io-biocontainers-thermorawfileparser-1.4.5--h05cac1d_1.img"
 
 # Check if the container image exists
 if [[ ! -f "$CONTAINER_IMAGE" ]]; then
